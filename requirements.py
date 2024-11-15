e5cf5c8689d919a622d24cbfde15b69d96dce849
Nauval Zulfikar <70472247+NauvalZulfikar@users.noreply.github.com>
2024-11-15 01:29:57 +0000

    requirements.py

--- requirements.py
+++ requirements.py
@@ -1,6 +1,6 @@
-streamlit 
-torch 
-torchvision 
-transformers 
-diffusers
-accelerate
+streamlit==1.25.0
+torch==2.0.1
+torchvision==0.15.2
+transformers==4.34.0
+diffusers==0.19.2
+accelerate==0.23.0
